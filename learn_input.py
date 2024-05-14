fbc1f77cfee2164ad48f44374d3ae3030a23922c
tmanor2604 <yamunaganapathy@gmail.com>
2024-05-14 11:59:12 +0530

    Add files via upload

---
+++ learn_input.py
@@ -0,0 +1,11 @@
+print (f'Welcome to the Band Name Generator')
+print (f'What\'s name of the city you grew up in?')
+
+city = input()
+print(city)
+
+print (f'What\'s your pet\'s name?')
+pet_name = input()
+print(pet_name)
+
+print(f'Your Band name could be {city} {pet_name}')
